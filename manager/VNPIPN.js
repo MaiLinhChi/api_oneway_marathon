89cc8a2eea30994d77f865b638e86af661c5da6c
iMac <imac@iMacs-iMac.local>
2023-04-18 18:19:13 +0700

    payment

--- manager/VNPIPN.js
+++ manager/VNPIPN.js
@@ -21,30 +21,19 @@ module.exports = {
         console.log(signed)
         console.log(req.query.vnp_SecureHash)
         console.log(req.query.vnp_SecureHash == signed)
+        const paymentedModel = await BibModel.findOne({txnRef: req.query.vnp_TxnRef});
+        if (req.query.vnp_SecureHash !== signed) return {message: 'Invalid signature', status: 400, RspCode: 97};
+        if (!paymentedModel) return {message: 'Order not found', status: 400, RspCode: 1};
+        if(paymentedModel.price !== req.query.vnp_Amount) return {message: 'Invalid amount', status: 400, RspCode: 4};
+        if (paymentedModel.status === "comfirmed") return {message: 'Order already confirmed', status: 400, RspCode: 2};
         const model = new IpnModel({
             txnRef : req.query.vnp_TxnRef,
             status : req.query.vnp_TransactionStatus == '00' ? 'success' : 'error',
             data: req.query
         })
-        const ipnSaved = await model.save();
-        const paymentedModel = await BibModel.findOne({txnRef: req.query.vnp_TxnRef});
-        if(ipnSaved) {
-            if (req.query.vnp_SecureHash === signed) {
-                if (!paymentedModel) {
-                    if(paymentedModel.price === req.query.vnp_Amount) {
-                        if (paymentedModel.status !== "comfirmed") {
-                            await BibModel.findByIdAndUpdate({txnRef: req.query.vnp_TxnRef}, {status: "confirmed"});
-                            return {message: 'Confirm Success', status: 400, RspCode: 0, data: ipnSaved};
-                        }
-                        return {message: 'Order already confirmed', status: 400, RspCode: 2};
-                    }
-                    return {message: 'Invalid amount', status: 400, RspCode: 4};
-                }
-                return {message: 'Order not found', status: 400, RspCode: 1};
-            }
-            return {message: 'Invalid signature', status: 400, RspCode: 97};
-        }
-        return {message: 'Unknow error', status: 400, RspCode: 99};
+        await BibModel.findByIdAndUpdate({txnRef: req.query.vnp_TxnRef}, {status: "confirmed"});
+        await model.save();
+        return {message: 'Confirm Success', status: 400, RspCode: 0};
     }
 }
 
