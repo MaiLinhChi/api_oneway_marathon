b4e8d8b5b65657f9434b784472af11d371a4cf16
taint-shopdi <tai.nguyen@shopdi.com.vn>
2023-05-10 23:10:18 +0700

    remove validate role when creating new group

--- route/Group.js
+++ route/Group.js
@@ -98,7 +98,6 @@ module.exports = {
             fullName: Joi.string().required(),
             email: Joi.string().required(),
             phone: Joi.string().required(),
-            role: Joi.string().required(),
         }),
         headers: Joi.object({
             authorization: Joi.string().required()
