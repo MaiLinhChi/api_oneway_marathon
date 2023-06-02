bfd9b283ee7f392d3a1555e48e3a11f81ae31cfc
tai.nguyen <tai.nguyen@shopdi.com.vn>
2023-06-02 14:35:54 +0700

    send mail when update member in group

--- email/index.js
+++ email/index.js
@@ -1,6 +1,8 @@
 const sendEmailRemoveMember = require('./remove-member')
 const sendEmailDeleteGroup = require('./delete-group')
+const sendEmailUpdateMember = require('./update-member')
 module.exports = {
     sendEmailRemoveMember,
-    sendEmailDeleteGroup
+    sendEmailDeleteGroup,
+    sendEmailUpdateMember
 }
