a908fa992acef8a339fb38712fdb67b73004f6b6
iMac <imac@iMacs-iMac.local>
2023-06-08 11:05:49 +0700

    fix marathon

--- route/Marathon.js
+++ route/Marathon.js
@@ -15,9 +15,9 @@ module.exports = {
             scope: ['admin']
         },
         validate: {
-            // headers: Joi.object({
-            //     authorization: Joi.string().required()
-            // }).options({allowUnknown: true}),
+            headers: Joi.object({
+                authorization: Joi.string().required()
+            }).options({allowUnknown: true}),
             params: Joi.object({
                 id: Joi.string().required(),
             }),
@@ -25,65 +25,64 @@ module.exports = {
                 name: Joi.string().optional(),
                 image: Joi.string().optional(),
                 imageEmail: Joi.string().optional(),
-                location: Joi.string().optional(),
-                type: Joi.string().default('Road/City trail').optional(),
                 description: Joi.string().optional(),
-                status: Joi.string().valid('active', 'inactive').optional(),
                 startTime: Joi.string().optional(),
+                status: Joi.string().optional(),
+                location: Joi.string().optional(),
+                type: Joi.string().optional().default('Road/City trail'),
                 unitRace: Joi.string().optional(),
                 race: Joi.array().min(1).items(Joi.object({
                     image: Joi.string().optional(),
                     distance: Joi.number().optional().description('race track length in kilomets'),
                     routeMap: Joi.string().optional().description('image router map'),
                     award: Joi.object({
-                        male: Joi.number().min(1000).optional().description('award on vnd'),
-                        female: Joi.number().min(1000).optional().description('award on vnd'),
+                        male: Joi.number().optional().description('award on vnd'),
+                        female: Joi.number().optional().description('award on vnd'),
                     }),
                     _id: Joi.string().optional(),
-                })).optional(),
+                })),
                 priceList: Joi.array().min(1).items(Joi.object({
                     name: Joi.string().optional(),
                     startSell: Joi.string().optional(),
                     endSell: Joi.string().optional(),
                     individual: Joi.array().min(1).items(Joi.object({
                         distance: Joi.number().optional(),
-                        price: Joi.number().min(1000).optional().description('price tiket on vnd'),
+                        price: Joi.number().optional(),
                         _id: Joi.string().optional(),
-                    })).optional(),
+                    })),
                     _id: Joi.string().optional(),
-                })).optional(),
+                })),
                 registerGroup: Joi.array().min(1).items(Joi.object({
                     numberPerson: Joi.object({
-                        from: Joi.number(),
-                        to: Joi.number(),
-                    }).optional(),
+                        from: Joi.number().optional(),
+                        to: Joi.number().optional(),
+                    }),
                     percent: Joi.number().optional(),
                     _id: Joi.string().optional(),
-                })).optional(),
+                })),
                 raceKit: Joi.array().min(1).max(4).items(Joi.object({
                     image: Joi.string().optional(),
-                    _id: Joi.string().optional(),
-                })).optional(),
+                })),
                 service: Joi.object({
                     image: Joi.string().optional(),
                     description: Joi.string().optional(),
-                }).optional(),
+                }),
                 schedule: Joi.array().min(1).items(Joi.object({
                     title: Joi.string().optional(),
                     description: Joi.string().optional(),
                     detail: Joi.array().min(1).items(Joi.object({
-                        time: Joi.string().optional().optional(),
-                        description: Joi.string().optional().optional(),
+                        time: Joi.string().optional(),
+                        description: Joi.string().optional(),
                         _id: Joi.string().optional(),
                     })),
                     _id: Joi.string().optional(),
-                })).optional(),
+                })),
                 regulation: Joi.array().min(1).items(Joi.object({
                     title: Joi.string().optional(),
                     description: Joi.string().optional(),
                     _id: Joi.string().optional(),
-                })).optional(),
-            })
+                })),
+            }),
         }
     },
     getById: {
@@ -150,21 +149,21 @@ module.exports = {
                 unitRace: Joi.string(),
                 race: Joi.array().min(1).items(Joi.object({
                     image: Joi.string(),
-                    distance: Joi.string().description('race track length in kilomets'),
+                    distance: Joi.number().description('race track length in kilomets'),
                     routeMap: Joi.string().description('image router map'),
                     award: Joi.object({
-                        male: Joi.string().description('award on vnd'),
-                        female: Joi.string().description('award on vnd'),
+                        male: Joi.number().description('award on vnd'),
+                        female: Joi.number().description('award on vnd'),
                     }),
                 })),
                 priceList: Joi.array().min(1).items(Joi.object({
                     name: Joi.string(),
                     startSell: Joi.string(),
                     endSell: Joi.string(),
-                    individual: Joi.object({
-                        distance: Joi.string(),
-                        price: Joi.string().description('price tiket on vnd'),
-                    }),
+                    individual: Joi.array().min(1).items(Joi.object({
+                        distance: Joi.number(),
+                        price: Joi.number(),
+                    })),
                 })),
                 registerGroup: Joi.array().min(1).items(Joi.object({
                     numberPerson: Joi.object({
@@ -183,10 +182,10 @@ module.exports = {
                 schedule: Joi.array().min(1).items(Joi.object({
                     title: Joi.string(),
                     description: Joi.string(),
-                    detail: Joi.object({
+                    detail: Joi.array().min(1).items(Joi.object({
                         time: Joi.string(),
                         description: Joi.string(),
-                    }),
+                    })),
                 })),
                 regulation: Joi.array().min(1).items(Joi.object({
                     title: Joi.string(),
